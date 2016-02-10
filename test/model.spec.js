aa7ea694f92923916ce9d0c16cb04065bd4f3c45
Jiri Spac <capajj@gmail.com>
2016-02-10 01:26:32 +0100

    validation and other functionality fleshed out

---
+++ test/model.spec.js
@@ -0,0 +1,24 @@
+/* eslint-env node, mocha */
+'use strict'
+
+describe('model', function () {
+  it('should give me a constructor I can use to create entities of certain schema', function () {
+
+  })
+
+  it('entites are observables which are saved with "put" on any change', function () {
+
+  })
+
+  it('entities can be removed and doing so removes them from the backup by calling "del" method', function () {
+
+  })
+
+  it('should validate any change against the schema', function (done) {
+
+  })
+
+  it('should allow a special "reference type" which gets automatically populated by observable instances on startup', function (done) {
+
+  })
+})
