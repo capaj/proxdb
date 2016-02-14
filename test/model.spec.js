8f3be185696492da137acc8e1c0475f139829a7e
Jiri Spac <capajj@gmail.com>
2016-02-14 19:39:23 +0100

    got rid of obsArray, changed ids and
    also implemented remove test

--- test/model.spec.js
+++ test/model.spec.js
@@ -31,12 +31,10 @@ describe('model', function () {
   })
 
   it('should save the object upon creation into backing store', function () {
-    expect(backingStore.callLog.put[0]).to.eql({
-      "doc": {
-        "birth": 1965,
-        "name": "A.C.Clarke"
-      },
-      "id": "8b68eaf153c763eb86885cc810142cd1ad7f53cc"
+    expect(backingStore.callLog.put[0].id).to.match(/Z8b68eaf153c763eb8688/)
+    expect(backingStore.callLog.put[0].doc).to.eql({
+      "birth": 1965,
+      "name": "A.C.Clarke"
     })
   })
 
@@ -47,12 +45,9 @@ describe('model', function () {
 
   it('entites are observables which are saved with "put" on any change', function () {
     clarke.birth = 1917 // he was actually born 1917
-    expect(backingStore.callLog.put[1]).to.eql({
-      "doc": {
-        "birth": 1917,
-        "name": "A.C.Clarke"
-      },
-      "id": "8b68eaf153c763eb86885cc810142cd1ad7f53cc"
+    expect(backingStore.callLog.put[1].doc).to.eql({
+      "birth": 1917,
+      "name": "A.C.Clarke"
     })
   })
 
@@ -60,12 +55,18 @@ describe('model', function () {
 
   })
 
-  it('entities can be removed and doing so removes them from the backup by calling "del" method', function () {
-
-  })
 
   it('should validate any change against the schema', function () {
+    try {
+      clarke.name = 42
+    } catch (err) {
+      expect(err.toString()).to.equal('ValidationError: "value" must be a string')
+    }
+  })
 
+  it('entities can be removed and doing so removes them from the backup by calling "del" method', function () {
+    clarke.remove()
+    expect(backingStore.callLog.del[0].id).to.match(/Z8b68eaf153c763eb8688/)
   })
 
   it('should allow a special "reference type" ref which gets automatically populated by observable instances on startup', function () {
